11c594eb061231323719cf95bc167b54384271fb
Ernest Talagtag <ernestneil09@gmail.com>
2025-09-25 01:21:46 +0800

    reviewing external libraries

--- Student.js
+++ Student.js
@@ -1,57 +1,81 @@
-let studentInformation = [];
+// let studentInformation = [];
 
-class Student{
-    constructor(studentObj){
-        this.studentObj = studentObj;
-    }
+// class Student{
+//     constructor(studentObj){
+//         this.studentObj = studentObj;
+//     }
 
-    Add(studentObj){
-        this.studentObj = studentObj;
-    }
+//     Add(studentObj){
+//         this.studentObj = studentObj;
+//     }
 
-    Display(){
-        console.log(this.studentObj);
-    }
+//     Display(){
+//         console.log(this.studentObj);
+//     }
+// }
+
+// let idElement = document.querySelector('.js-input-id-text');
+// let nameElement = document.querySelector('.js-input-name-text');
+// let programElement = document.querySelector('.js-input-program-text');
+// let ageElement = document.querySelector('.js-input-age-text');
+// let addressElement = document.querySelector('.js-input-address-text');
+
+// let submitButton = document.querySelector('.js-submit');
+// let displayButton = document.querySelector('.js-display');
+
+// submitButton.addEventListener('click', () => {
+//     let studentObj = {
+//         id: idElement.value,
+//         name: nameElement.value,
+//         program: programElement.value,
+//         age: ageElement.value,
+//         address: addressElement.value
+//     };
+//     const student = new Student(studentObj);
+//     studentInformation.push(studentObj);
+
+//     student.Display();
+//     console.log(studentInformation);
+// });
+
+// displayButton.addEventListener('click', () => {
+//     let HTML = '';
+//     studentInformation.forEach((student, index) => {
+//         HTML +=`
+//         ID: ${studentInformation[index]['id']} <br>
+//         Name: ${studentInformation[index]['name']} <br>
+//         Program: ${studentInformation[index]['program']} <br>
+//         Age: ${studentInformation[index]['age']} <br>
+//         Address: ${studentInformation[index]['address']}
+//         <br>
+//         <br>
+//         `
+        
+//     });
+//     document.querySelector('.js-display-text').innerHTML = HTML;
+//     ;
+// });
+
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+
+const today = dayjs();
+// const nextMonth = today.add(1, 'month');
+// const getBackMonth = nextMonth.subtract(1, 'M');
+// const nextMonthFormatted = getBackMonth.format('dddd');
+
+// console.log(nextMonthFormatted);
+
+function isWeekend(date){
+    
+    const today = date;
+    const nextDay = today.add(3, 'd');
+    const nextDayFormatted = nextDay.format('dddd');
+
+    if(nextDayFormatted == 'Saturday' || nextDayFormatted == 'Sunday'){
+        return true;
+    }    
+
+    return false;
 }
 
-let idElement = document.querySelector('.js-input-id-text');
-let nameElement = document.querySelector('.js-input-name-text');
-let programElement = document.querySelector('.js-input-program-text');
-let ageElement = document.querySelector('.js-input-age-text');
-let addressElement = document.querySelector('.js-input-address-text');
-
-let submitButton = document.querySelector('.js-submit');
-let displayButton = document.querySelector('.js-display');
-
-submitButton.addEventListener('click', () => {
-    let studentObj = {
-        id: idElement.value,
-        name: nameElement.value,
-        program: programElement.value,
-        age: ageElement.value,
-        address: addressElement.value
-    };
-    const student = new Student(studentObj);
-    studentInformation.push(studentObj);
-
-    student.Display();
-    console.log(studentInformation);
-});
-
-displayButton.addEventListener('click', () => {
-    let HTML = '';
-    studentInformation.forEach((student, index) => {
-        HTML +=`
-        ID: ${studentInformation[index]['id']} <br>
-        Name: ${studentInformation[index]['name']} <br>
-        Program: ${studentInformation[index]['program']} <br>
-        Age: ${studentInformation[index]['age']} <br>
-        Address: ${studentInformation[index]['address']}
-        <br>
-        <br>
-        `
-        
-    });
-    document.querySelector('.js-display-text').innerHTML = HTML;
-    ;
-});
+console.log(isWeekend(today));
